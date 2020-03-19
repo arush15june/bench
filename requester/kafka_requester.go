15cf0d83e4eb4aa49a1fcdd360dab9254a3d4e95
Aarush Ahuja <ahujaarush@gmail.com>
2020-03-20 03:20:05 +0530

    Update kafka_requester.go

--- requester/kafka_requester.go
+++ requester/kafka_requester.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"github.com/Shopify/sarama"
-	"github.com/tylertreat/bench"
+	"github.com/arush15june/bench"
 )
 
 // KafkaRequesterFactory implements RequesterFactory by creating a Requester
