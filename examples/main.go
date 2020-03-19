cc1b5c4ee7e60151005e952e4254e411f8f1f300
charge <ahujaarush@gmail.com>
2020-03-20 03:26:03 +0530

    fix import

--- examples/main.go
+++ examples/main.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/tylertreat/bench"
-	"github.com/tylertreat/bench/requester"
+	"github.com/arush15june/bench"
+	"github.com/arush15june/bench/requester"
 )
 
 func main() {
